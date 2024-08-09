36c75f6af3682a6909a17d0e0b0ef419fe16ad1e
Amos Egonmwan <48167887+amosegonmwan@users.noreply.github.com>
2024-08-09 12:02:29 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "git@github.com:amosegonmwan/github-actions-102.git//s3-module?ref=v2.1.0"
+  source = "git@github.com:amosegonmwan/github-actions-102.git//s3-module?ref=v2.6.0"
   env = "prod"
   topic_name = "wema"
   my_bucket = "devops"
